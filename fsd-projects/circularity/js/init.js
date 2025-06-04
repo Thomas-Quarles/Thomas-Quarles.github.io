1812c61d79a32385f65b23db99c75cdf96aa4d3a
Thomas-Quarles <quarles55886@students.apsb.org>
2025-06-04 16:09:18 +0000

    complete circularity

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -26,7 +26,7 @@ var init = function (window) {
 
         // TODO 2 : Create a function that draws a circle 
         function drawCircle(){
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            circle = draw.randomCircleInArea(canvas, true, false, "red", 3);
             physikz.addRandomVelocity(circle, canvas, 5, 5);
             view.addChild(circle);
             circles.push(circle);
